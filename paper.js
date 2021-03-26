6f10868cd9076727a68ac98f9c2dc58159ffd7ab
Subhrajyotsna <75079896+Subhrajyotsna@users.noreply.github.com>
2021-03-26 22:08:16 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,40 @@
+class Rubber{
+	constructor(x,y,r)
+	
+	{
+		var options={
+            isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2,
+	
+			
+		}
+		this.x=x;
+		this.y=y;
+		this.r=r;
+        
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.image = loadImage("paper.png");
+		World.add(world, this.body);
+		
+
+       
+            
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(2);
+			stroke("black");
+			fill("#FA86C4");
+			ellipse(0,0,this.r, this.r);
+			pop()
+	}
+	
+
+}
