2542b50c54f48572dff6b055f76bfb00e332c7cd
Subhrajyotsna <75079896+Subhrajyotsna@users.noreply.github.com>
2021-03-31 12:04:42 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,65 +1,42 @@
-class dustbin
-{
-	constructor(x,y)
+class Rubber{
+	constructor(x,y,r)
+	
 	{
+		var options={
+            isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2,
+			
+	
+			
+		}
 		this.x=x;
 		this.y=y;
-		this.dustbinWidth=200;
-		this.dustbinHeight=213;
-		this.wallThickness=20;
+		this.r=r;
+        
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
 		
-		this.image=loadImage("dustbingreen.png")
-		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
-		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-		
+		World.add(world, this.body);
+		this.image = loadImage("paper.png");
 		
 
-		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-		
-		World.add(world, this.bottomBody)
-		World.add(world, this.leftWallBody)
-		World.add(world, this.rightWallBody);
+       
+            
 
 	}
 	display()
 	{
-			var posBottom=this.bottomBody.position;
-			var posLeft=this.leftWallBody.position;
-			var posRight=this.rightWallBody.position;
-
-			
-
+			var rubberpos=this.body.position;		
 			push()
-			translate(posLeft.x, posLeft.y);
+			translate(rubberpos.x, rubberpos.y);
 			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			rotate(this.angle)
-			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			strokeWeight(2);
+			stroke("black");
+			fill("#FA86C4");
+			image(this.image,0,0,this.r, this.r);
 			pop()
-
-			push()
-			translate(posRight.x, posRight.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			rotate(-1*this.angle)
-			
-			pop()
-
-			push()
-			translate(posBottom.x, posBottom.y+10);
-			rectMode(CENTER)
-			
-			angleMode(RADIANS)
-			fill(255)
-			imageMode(CENTER);
-			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
-			
-			pop()
-			
 	}
+	
 
 }
